40a9cf4e761ecc34805b510562e09536ef6d5393
Kakasei <CitrusKakasei@outlook.com>
2026-04-05 19:47:44 +0800

    修复导航栏路径

--- components/CustomNavBar.vue
+++ components/CustomNavBar.vue
@@ -2,15 +2,17 @@
   <!-- 顶部导航栏 -->
   <header class="sticky top-0 p-3 bg-white pointer-events-auto z-50 border-b-2 border-gray-100">
     <div class="flex justify-between max-w-7xl mx-auto items-center">
-      <a href="/index" class="text-2xl font-bold">Postscript</a>
+      <a :href="withBase('/')" class="text-2xl font-bold">Postscript</a>
       <nav class="flex">
-        <a href="/index" class="px-4">首页</a>
-        <a href="/about" class="px-4">关于作者</a>
+        <a :href="withBase('/')" class="px-4">首页</a>
+        <a :href="withBase('/about')" class="px-4">关于作者</a>
       </nav>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { withBase } from "vitepress";
+</script>
 
 <style scoped></style>
